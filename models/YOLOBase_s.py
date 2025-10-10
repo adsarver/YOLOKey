4f10b63ce11973f469f0cc5ee23dd5d290d99981
asarver <andsarve15@gmail.com>
2025-10-09 20:53:12 -0400

    Lowered baseline to YOLOv9t

--- models/YOLOBase_s.py
+++ models/YOLOBase_s.py
@@ -1,11 +1,11 @@
 import math
 import torch
 import torch.nn as nn
-from utils.model_utils import Conv, RepNCSPELAN4, MConv, SPPELAN, ELAN1, Concat, DualDDetect, meta
+from utils.model_utils import Conv, RepNCSPELAN4, AConv, SPPELAN, ELAN1, Concat, DualDDetect, meta
 
 # --- Main YOLOv9 Model ---
 
-class YOLOMax(nn.Module):
+class YOLOBase(nn.Module):
     """
     YOLOv9 model implemented in PyTorch based on 
     https://github.com/WongKinYiu/yolov9/ YAML configuration.
@@ -16,45 +16,45 @@ class YOLOMax(nn.Module):
         self.hyp = meta  # Placeholder for hyperparameters if needed
 
         # --- Backbone ---
-        self.b0 = Conv(ch, 16, 3, 2)                            # 0
-        self.b1 = Conv(16, 32, 3, 2)                            # 1
-        self.b2 = ELAN1(32, 32, 32, 16)                         # 2
-        self.b3 = MConv(32, 64)                                 # 3
-        self.b4 = RepNCSPELAN4(64, 64, 64, 32, 3)               # 4
-        self.b5 = MConv(64, 96)                                 # 5
-        self.b6 = RepNCSPELAN4(96, 96, 96, 48, 3)               # 6
-        self.b7 = MConv(96, 128)                                # 7
-        self.b8 = RepNCSPELAN4(128, 128, 128, 64, 3)            # 8
-
+        self.b0 = Conv(ch, 32, 3, 2)                            # 0
+        self.b1 = Conv(32, 64, 3, 2)                            # 1
+        self.b2 = ELAN1(64, 64, 32, 1)                          # 2
+        self.b3 = AConv(64, 128)                                # 3
+        self.b4 = RepNCSPELAN4(128, 128, 128, 64, 3)            # 4
+        self.b5 = AConv(128, 192)                               # 5
+        self.b6 = RepNCSPELAN4(192, 192, 192, 96, 3)            # 6
+        self.b7 = AConv(192, 256)                               # 7
+        self.b8 = RepNCSPELAN4(256, 256, 256, 128, 3)           # 8
+        
         # --- Head ---
-        self.h9 = SPPELAN(128, 128, 64)                         # 9
+        self.h9 = SPPELAN(256, 256, 128)                        # 9
         self.h10 = nn.Upsample(scale_factor=2, mode='nearest')  # 10
         self.h11 = Concat(1)                                    # 11
-        self.h12 = RepNCSPELAN4(192, 96, 96, 48, 3)             # 12
+        self.h12 = RepNCSPELAN4(448, 192, 192, 96, 3)           # 12
         self.h13 = nn.Upsample(scale_factor=2, mode='nearest')  # 13
         self.h14 = Concat(1)                                    # 14
-        self.h15 = RepNCSPELAN4(160, 64, 64, 32, 3)             # 15 (P3 for detection)
-        self.h16 = MConv(64, 48)                                # 16
+        self.h15 = RepNCSPELAN4(320, 128, 128, 64, 3)           # 15 (P3 for detection)
+        self.h16 = AConv(128, 96)                               # 16
         self.h17 = Concat(1)                                    # 17
-        self.h18 = RepNCSPELAN4(208, 96, 96, 48, 3)             # 18 (P4/16 for detection)
-        self.h19 = MConv(96, 64)                                # 19
+        self.h18 = RepNCSPELAN4(288, 192, 192, 96, 3)           # 18 (P4/16 for detection)
+        self.h19 = AConv(192, 128)                              # 19
         self.h20 = Concat(1)                                    # 20
-        self.h21 = RepNCSPELAN4(272, 128, 128, 64, 3)           # 21 (P5/32 for detection)
-        self.h22 = SPPELAN(128, 128, 64)                        # 22
+        self.h21 = RepNCSPELAN4(384, 256, 256, 128, 3)          # 21 (P5/32 for detection)
+        self.h22 = SPPELAN(256, 256, 128)                       # 22
         self.h23 = nn.Upsample(scale_factor=2, mode='nearest')  # 23
         self.h24 = Concat(1)                                    # 24
-        self.h25 = RepNCSPELAN4(192, 96, 96, 48, 3)             # 25
+        self.h25 = RepNCSPELAN4(448, 192, 192, 96, 3)           # 25
         self.h26 = nn.Upsample(scale_factor=2, mode='nearest')  # 26
         self.h27 = Concat(1)                                    # 27
-        self.h28 = RepNCSPELAN4(160, 64, 64, 32, 3)             # 28
+        self.h28 = RepNCSPELAN4(320, 128, 128, 64, 3)           # 28
 
-        ch_detect = [64, 96, 128, 64, 96, 128]
+        ch_detect = [128, 192, 256, 128, 192, 256]
         self.h29 = DualDDetect(nc, ch=ch_detect)                # 29
         
         self.model = nn.ModuleList([
             self.b0, self.b1, self.b2, self.b3, self.b4, self.b5, self.b6, self.b7, self.b8, self.h9, # 0-9
             self.h10, self.h11, self.h12, self.h13, self.h14, self.h15, self.h16, self.h17, self.h18, self.h19, # 10-19
-            self.h20, self.h21, self.h22, self.h23, self.h24, self.h25, self.h26, self.h27, self.h28, self.h29 # 20-29
+            self.h20, self.h21, self.h22, self.h23, self.h24, self.h25, self.h26, self.h27, self.h28, self.h28, self.h29 # 20-29
         ])
 
     def forward(self, x):
