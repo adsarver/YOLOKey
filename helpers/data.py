Fixed image saving with normalization, fixed dfl and box loss issues

helpers/data.py
@@ -48,8 +48,7 @@ class YoloDataset(Dataset):
             with open(label_path, 'r') as f:
                 for line in f:
                     parts = line.strip().split()
-                    class_id = int(parts[0])
-                    
+
                     # Bbox format: [class_id, x_center, y_center, width, height]
                     bbox = [float(p) for p in parts[:5]]
                     
@@ -58,12 +57,6 @@ class YoloDataset(Dataset):
 
         if self.transform:
             img = self.transform(img)
-        else:
-            # Default transform: convert to tensor and normalize
-            img = torch.from_numpy(img.transpose(2, 0, 1)).float() / 255.0
-
-        # Adjust bounding boxes for resizing if needed (not necessary for normalized coords)
-        # In a real scenario, augmentations would also need to transform the bounding boxes.
 
         return img, labels
 

helpers/train.py
@@ -94,16 +94,18 @@ def load_weights(model, weights_path):
         pretrained_weights = checkpoint['model'].float().state_dict()
         custom_model_dict = model.state_dict()
         mismatch = []
+        match = []
         weights_to_load = {}
         for name, param in pretrained_weights.items():
             if name in custom_model_dict and param.shape == custom_model_dict[name].shape:
                 weights_to_load[name] = param
+                match.append(name)
             else:
                 mismatch.append((name, param.shape, custom_model_dict[name].shape if name in custom_model_dict else 'Not Found'))
                 pass
 
         if mismatch:
-            print(f"Weights loading completed with {len(mismatch)} mismatches.")
+            print(f"Weights loading completed with {len(match)} matches and {len(mismatch)} mismatches.")
 
         model.load_state_dict(weights_to_load, strict=False)
         print(f"Weights loaded from {weights_path}")
@@ -135,9 +137,9 @@ def train(config, model, weights_path=None):
     niou = iouv.numel()
     
     # --- Data Augmentation ---
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
     trtransforms = v2.Compose([
         v2.ToImage(),  # Convert to tensor, only needed if you had a PIL image
         v2.ToDtype(torch.uint8, scale=True),  # optional, most input are already uint8 at this point
@@ -209,8 +211,8 @@ def train(config, model, weights_path=None):
             pbar_train.set_postfix({
             'Loss': f"{loss.item():.4f}",
             'Box': f"{components[0]:.4f}",
-            'Obj': f"{components[1]:.4f}",
-            'Cls': f"{components[2]:.4f}",
+            'Cls': f"{components[1]:.4f}",
+            'Dfl': f"{components[2]:.4f}",
             'VRAM': f"{torch.cuda.memory_reserved()/1E9 if torch.cuda.is_available() else 0:.2f}GB"
         })
         
@@ -236,9 +238,9 @@ def train(config, model, weights_path=None):
                 
                 preds = non_max_suppression(preds[0], conf_thres=0.001, iou_thres=0.7, labels=data_config.get('labels'), multi_label=True, agnostic=False, max_det=100)
                 # Plot images
-                if (epoch+1 % 20 == 0 or epoch == 0) and len(stats) == 0:
-                    log_random_image_predictions(model, val_loader, device, run_dir, epoch, data_config['names'])
-                    
+                if (epoch+1 % 1 == 0 or epoch == 0) and len(stats) == 0:
+                    log_random_image_predictions(images, targets, preds, run_dir, epoch, data_config['names'])
+
                 for si, pred in enumerate(preds):
                     labels = torch.Tensor(targets[targets[:, 0] == si, 1:]).to(device)  # labels for image si
                     nl, npr = labels.shape[0], pred.shape[0]  # number of labels, predictions
@@ -265,11 +267,11 @@ def train(config, model, weights_path=None):
                 pbar_val.set_postfix({
                     'Loss': f"{loss.item():.4f}",
                     'Box': f"{components[0]:.4f}",
-                    'Obj': f"{components[1]:.4f}",
-                    'Cls': f"{components[2]:.4f}",
+                    'Cls': f"{components[1]:.4f}",
+                    'Dfl': f"{components[2]:.4f}",
                     'VRAM': f"{torch.cuda.memory_reserved()/1E9 if torch.cuda.is_available() else 0:.2f}GB"
-                })                       
-         
+                })
+        
         avg_val_loss = val_loss / len(val_loader)
         
         # Compute metrics
@@ -316,7 +318,7 @@ if __name__ == '__main__':
         'img_size': 128,
         'batch_size': 64,
         'epochs': 500,
-        'learning_rate': 0.001
+        'learning_rate': 0.005
     }
-    train(config, YOLOBase, 'yolov9-t-converted.pt')
+    train(config, YOLOMax, 'yolov9-t-converted.pt')
 

models/YOLOMax.py
@@ -1,10 +1,9 @@
 import math
 import torch
 import torch.nn as nn
-from utils.model_utils import Conv, RepNCSPELAN4, MConv, SPPELAN, ELAN1, Concat, DualDDetect, meta
+from utils.model_utils import *
 
 # --- Main YOLOv9 Model ---
-
 class YOLOMax(nn.Module):
     """
     YOLOv9 model implemented in PyTorch based on 
@@ -12,6 +11,7 @@ class YOLOMax(nn.Module):
     """
     def __init__(self, nc, ch=3):
         super().__init__()
+        print(f"Building YOLOv9 Base model with {nc} classes")
         self.nc = nc
         self.hyp = meta  # Placeholder for hyperparameters if needed
 
@@ -49,12 +49,18 @@ class YOLOMax(nn.Module):
         self.h28 = RepNCSPELAN4(160, 64, 64, 32, 3)             # 28
 
         ch_detect = [64, 96, 128, 64, 96, 128]
-        self.h29 = DualDDetect(nc, ch=ch_detect)                # 29
+        self.detect = DualDDetect(nc, ch=ch_detect)             # 29
+        
+        s = 256  # 2x min stride
+        forward = lambda x: self.forward(x)[0]
+        self.detect.stride = torch.tensor([s / x.shape[-2] for x in forward(torch.zeros(1, ch, s, s))])  # forward
+        self.stride = self.detect.stride
+        self.detect.bias_init()  # only run once
         
         self.model = nn.ModuleList([
             self.b0, self.b1, self.b2, self.b3, self.b4, self.b5, self.b6, self.b7, self.b8, self.h9, # 0-9
             self.h10, self.h11, self.h12, self.h13, self.h14, self.h15, self.h16, self.h17, self.h18, self.h19, # 10-19
-            self.h20, self.h21, self.h22, self.h23, self.h24, self.h25, self.h26, self.h27, self.h28, self.h29 #20-29
+            self.h20, self.h21, self.h22, self.h23, self.h24, self.h25, self.h26, self.h27, self.h28, self.detect #20-29
         ])
 
     def forward(self, x):
@@ -89,5 +95,5 @@ class YOLOMax(nn.Module):
         x27 = self.h27([x26, x4]) # 27
         x28 = self.h28(x27) # 28
         
-        return self.h29([x28, x25, x22, x15, x18, x21]) # 38 (P3, P4, P5 for detection)
+        return self.detect([x28, x25, x22, x15, x18, x21]) # 38 (P3, P4, P5 for detection)
         

utils/model_utils.py
@@ -1,20 +1,12 @@
-import ast
-import contextlib
-import json
 import math
-import platform
 import warnings
-import zipfile
-from collections import OrderedDict, namedtuple
 from copy import copy
 from pathlib import Path
-
-import cv2
+import torch.nn.functional as F
 import numpy as np
 import torch
 import torch.nn as nn
 from PIL import Image
-
 from utils.utils import colors
 
 meta = {
@@ -27,7 +19,7 @@ meta = {
     'anchor_t': (1, 2.0, 8.0),  # anchor-multiple threshold
     'anchors': (2, 2.0, 10.0),  # anchors per output grid (0 to ignore)
     'fl_gamma': 1.5,  # focal loss gamma (efficientDet default gamma=1.5)
-}  # segment copy-paste (probability)
+}
 
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
     # Pad to 'same' shape outputs
@@ -36,6 +28,7 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
+
 
 def dist2bbox(distance, anchor_points, xywh=True, dim=-1):
     """Transform distance(ltrb) to box(xywh or xyxy)."""
@@ -48,11 +41,13 @@ def dist2bbox(distance, anchor_points, xywh=True, dim=-1):
         return torch.cat((c_xy, wh), dim)  # xywh bbox
     return torch.cat((x1y1, x2y2), dim)  # xyxy bbox
 
+
 def make_divisible(x, divisor):
     # Returns nearest x divisible by divisor
     if isinstance(divisor, torch.Tensor):
         divisor = int(divisor.max())  # to int
     return math.ceil(x / divisor) * divisor
+
 
 def make_anchors(feats, strides, grid_cell_offset=0.5):
     """Generate anchors from features."""
@@ -68,6 +63,7 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
         stride_tensor.append(torch.full((h * w, 1), stride, dtype=dtype, device=device))
     return torch.cat(anchor_points), torch.cat(stride_tensor)
 
+
 class DFL(nn.Module):
     # DFL module
     def __init__(self, c1=17):
@@ -81,6 +77,7 @@ class DFL(nn.Module):
         b, c, a = x.shape  # batch, channels, anchors
         return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
         # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+
 
 class DualDDetect(nn.Module):
     # YOLO Detect head for detection models
@@ -144,6 +141,7 @@ class DualDDetect(nn.Module):
             a[-1].bias.data[:] = 1.0  # box
             b[-1].bias.data[:m.nc] = math.log(5 / m.nc / (640 / s) ** 2)  # cls (5 objects and 80 classes per 640 image)
 
+
 class Conv(nn.Module):
     # Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)
     default_act = nn.SiLU()  # default activation
@@ -169,7 +167,8 @@ class AConv(nn.Module):
     def forward(self, x):
         x = torch.nn.functional.avg_pool2d(x, 2, 1, 0, False, True)
         return self.cv1(x)
-    
+
+
 class MConv(nn.Module):
     def __init__(self, c1, c2):  # ch_in, ch_out, shortcut, kernels, groups, expand
         super().__init__()
@@ -512,7 +511,6 @@ class SPP(nn.Module):
 
         
 class ASPP(torch.nn.Module):
-
     def __init__(self, in_channels, out_channels):
         super().__init__()
         kernel_sizes = [1, 3, 3, 1]
@@ -586,10 +584,6 @@ class SPPF(nn.Module):
             y1 = self.m(x)
             y2 = self.m(y1)
             return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
-
-
-import torch.nn.functional as F
-from torch.nn.modules.utils import _pair
     
     
 class ReOrg(nn.Module):
@@ -655,8 +649,7 @@ class Silence(nn.Module):
         return x
 
 
-##### GELAN #####        
-        
+##### GELAN #####
 class SPPELAN(nn.Module):
     # spp-elan
     def __init__(self, c1, c2, c3):  # ch_in, ch_out, number, shortcut, groups, expansion
@@ -675,7 +668,6 @@ class SPPELAN(nn.Module):
         
         
 class ELAN1(nn.Module):
-
     def __init__(self, c1, c2, c3, c4):  # ch_in, ch_out, number, shortcut, groups, expansion
         super().__init__()
         self.c = c3//2
@@ -715,11 +707,8 @@ class RepNCSPELAN4(nn.Module):
         y.extend(m(y[-1]) for m in [self.cv2, self.cv3])
         return self.cv4(torch.cat(y, 1))
 
-#################
-
 
 ##### YOLOR #####
-
 class ImplicitA(nn.Module):
     def __init__(self, channel):
         super(ImplicitA, self).__init__()
@@ -741,8 +730,8 @@ class ImplicitM(nn.Module):
     def forward(self, x):
         return self.implicit * x
 
-##### CBNet #####
 
+##### CBNet #####
 class CBLinear(nn.Module):
     def __init__(self, c1, c2s, k=1, s=1, p=None, g=1):  # ch_in, ch_outs, kernel, stride, padding, groups
         super(CBLinear, self).__init__()
@@ -764,8 +753,8 @@ class CBFuse(nn.Module):
         out = torch.sum(torch.stack(res + xs[-1:]), dim=0)
         return out
 
-#################
 
+#################
 class Detections:
     # YOLO detections class for inference results
     def __init__(self, ims, pred, files, times=(0, 0, 0), names=None, shape=None):

utils/utils.py
@@ -10,48 +10,33 @@ import cv2
 from torchvision.utils import draw_bounding_boxes, save_image
 import random
 
-def un_normalize_image(tensor):
+def un_normalize_image(img):
     """Reverses the normalization on an image tensor."""
     # These are the standard ImageNet mean and std
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    
-    # Move tensor to CPU and convert to NumPy
-    img_np = tensor.cpu().numpy().transpose(1, 2, 0)
+    mean = torch.tensor([0.485, 0.456, 0.406], device=img.device).view(-1, 1, 1)
+    std = torch.tensor([0.229, 0.224, 0.225], device=img.device).view(-1, 1, 1)
     
     # Un-normalize
-    img_np = std * img_np + mean
-    img_np = np.clip(img_np, 0, 1)
+    img = img * std + mean
+    img = img.clamp(0, 1)
     
     # Convert to 8-bit unsigned integer format for image display
-    img_uint8 = (img_np * 255).astype(np.uint8)
+    img_uint8 = (img * 255).to(torch.uint8)
     
-    # Return a contiguous copy to avoid potential issues with OpenCV
-    return img_uint8.copy()
+    # img = img.permute(1, 2, 0)  # Change from CxHxW to HxWxC
+    
+    # Return a contiguous copy
+    return img_uint8.contiguous()
 
-def log_random_image_predictions(model, val_loader, device, run_dir, epoch, class_names):
+def log_random_image_predictions(images, targets, preds, run_dir, epoch, class_names):
     """Logs a random image with its ground truth and predicted bounding boxes."""
-    model.eval()
-    
-    # Get a single batch from the validation loader for visualization
-    try:
-        images, targets = next(iter(val_loader))
-    except StopIteration:
-        print("Validation loader is empty, cannot log image.")
-        return
-        
-    images = images.to(device)
-    
-    with torch.no_grad():
-        inference_preds = model(images)[0][1]
-
     # Select a random image from the batch
     img_idx = random.randint(0, images.shape[0] - 1)
     img_tensor = images[img_idx]
-    img_to_draw = un_normalize_image(img_to_draw)
+    img_to_draw = un_normalize_image(img_tensor)
 
     # Convert image tensor to uint8 for drawing
-    img_h, img_w = img_tensor.shape[1:]
+    img_h, img_w = img_to_draw.shape[1:]
 
     # --- Get and Format Ground Truth Boxes (Green) ---
     gt_targets = targets[targets[:, 0] == img_idx, 1:]
@@ -66,7 +51,7 @@ def log_random_image_predictions(model, val_loader, device, run_dir, epoch, clas
     gt_labels = [class_names[int(c)] for c in gt_targets[:, 0]]
 
     # --- Get and Format Predicted Boxes (Blue) ---
-    preds_for_img = inference_preds[img_idx]
+    preds_for_img = preds[img_idx]
     preds_for_img[:, 5:] *= preds_for_img[:, 4:5]  # conf = obj_conf * cls_conf
     
     vis_conf_thres = 0.25 
@@ -113,10 +98,6 @@ def xywh2xyxy(x):
 def is_parallel(model):
     # Returns True if model is of type DP or DDP
     return type(model) in (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)
-
-def de_parallel(model):
-    # De-parallelize a model: returns single-GPU model if model is of type DP or DDP
-    return model.module if is_parallel(model) else model
 
 def bbox2dist(anchor_points, bbox, reg_max):
     """Transform bbox(xyxy) to dist(ltrb)."""